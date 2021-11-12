bcf3922bfc6697cae643b61f6f6b4bfb6eb78bcc
Jonas Pedersen <jpe@kvalitetsit.dk>
2021-11-12 13:49:29 +0100

    Add logging in business logic.

--- service/src/main/java/dk/kvalitetsit/hello/service/HtmlServiceImpl.java
+++ service/src/main/java/dk/kvalitetsit/hello/service/HtmlServiceImpl.java
@@ -2,11 +2,15 @@ package dk.kvalitetsit.hello.service;
 
 import dk.kvalitetsit.hello.service.model.HtmlInfo;
 import dk.kvalitetsit.prometheus.app.info.actuator.VersionProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 
 public class HtmlServiceImpl implements HtmlService {
+    private static final Logger logger = LoggerFactory.getLogger(HtmlServiceImpl.class);
+
     private final VersionProvider VersionProvider;
     private final String configurableText;
 
@@ -17,6 +21,8 @@ public class HtmlServiceImpl implements HtmlService {
 
     @Override
     public HtmlInfo getHtmlInfo() throws UnknownHostException {
+        logger.info("Nu laver vi forretningslogik.");
+
         var htmlInfo = new HtmlInfo();
         htmlInfo.setVersion(VersionProvider.getVersion());
         htmlInfo.setHostName(Inet4Address.getLocalHost().getHostName());
