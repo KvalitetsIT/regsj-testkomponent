a173a591337b92804a0aa228ce99d1e86710da71
Jonas Pedersen <jpe@kvalitetsit.dk>
2021-11-12 11:20:21 +0100

    Push to harbor

--- build/failOnFirstBuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "${GITHUB_REPOSITORY}"
-echo "${DOCKER_SERVICE}"
-if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/regsj-testkomponent" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/regsj-testkomponent" ]; then
-  echo "Please run setup.sh REPOSITORY_NAME"
-  exit 1
-fi
--- documentation/docker/getOldDoc.sh
+++ documentation/docker/getOldDoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if docker pull kvalitetsit/regsj-testkomponent-documentation:latest; then
+if docker pull dockerregistry.regsj.hosting.kitkube.dk/regsj/regsj-testkomponent-documentation:latest; then
     echo "Copy from old documentation image."
-    docker cp $(docker create kvalitetsit/regsj-testkomponent-documentation:latest):/usr/share/nginx/html target/old
+    docker cp $(docker create regsj/regsj-testkomponent-documentation:latest):/usr/share/nginx/html target/old
 fi
