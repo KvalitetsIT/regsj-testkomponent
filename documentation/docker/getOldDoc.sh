fdc4d7c4134f2bfe4f4d3a1d45e8c66dff749a44
Jonas Pedersen <jpe@kvalitetsit.dk>
2021-11-15 13:54:46 +0100

    Correct documentation build.

--- documentation/docker/getOldDoc.sh
+++ documentation/docker/getOldDoc.sh
@@ -2,5 +2,5 @@
 
 if docker pull dockerregistry.regsj.hosting.kitkube.dk/regsj/regsj-testkomponent-documentation:latest; then
     echo "Copy from old documentation image."
-    docker cp $(docker create regsj/regsj-testkomponent-documentation:latest):/usr/share/nginx/html target/old
+    docker cp $(docker create dockerregistry.regsj.hosting.kitkube.dk/regsj/regsj-testkomponent-documentation:latest):/usr/share/nginx/html target/old
 fi
